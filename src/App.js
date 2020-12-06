4f16d982a849a70f3ee188021aff818a87eaf41b
Brian Sanchez <briansanchezizq@gmail.com>
2020-12-06 09:22:48 -0600

    Styled components en Formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { Fragment } from 'react';
-import Header from './Components/Header'
+import React from 'react';
+import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+import styled from '@emotion/styled';
+
+const Contenendor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
 function App() {
   return (
-    <Fragment>
+    <Contenendor>
       <Header
         titulo="Cotizador de seguros"/>
-    </Fragment>
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
+    </Contenendor>
   );
 }
 
